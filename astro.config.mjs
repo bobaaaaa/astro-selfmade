09104ca009af5d8df7770f0a856bc7fe86754e2e
Patrick Dahms <pat.dahms@googlemail.com>
2024-05-20 10:28:04 +0200

    add project base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://bobaaaaa.github.io',
+	base: 'astro-selfmade',
 	integrations: [tailwind()],
 });
